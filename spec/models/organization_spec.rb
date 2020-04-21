a404fbadd426c6320035b9b63ac096649756686a
boykj <jboyk918@gmail.com>
2020-04-21 11:40:05 -0700

    org_spec.rb: Add to_s method test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-	let (:org) {Organization.new}
+	let (:org) {Organization.new(name: 'FAKE')}
 
 	describe "relationships" do
 		it "has many users" do
@@ -16,23 +16,33 @@ RSpec.describe Organization, type: :model do
 	end
 	
 	describe "properties" do
-		it "has a name" do
-			expect(org).to respond_to(:name)
-		end
 		it "has an email" do
 			expect(org).to respond_to(:email)
 		end
 		it "has a name" do
+			expect(org).to respond_to(:name)
+		end
+		it "has a phone" do
+			expect(org).to respond_to(:phone)
+		end
+		it "has a status" do
 			expect(org).to respond_to(:status)
 		end
-		it "has a name" do
+		it "has a primary name" do
 			expect(org).to respond_to(:primary_name)
 		end
-		it "has a name" do
+		it "has a secondary name" do
 			expect(org).to respond_to(:secondary_name)
 		end
-		it "has a name" do
+		it "has a secondary phone" do
 			expect(org).to respond_to(:secondary_phone)
+		end
+	end
+
+	describe "#to_s" do
+		it "has a string representation that is a name" do
+			expected_name = 'FAKE'
+			expect(org.to_s).to eq(expected_name)
 		end
 	end
 		
