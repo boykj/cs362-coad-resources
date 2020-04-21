84c421abe9babdf08c25191d6e379cb2b882086d
boykj <jboyk918@gmail.com>
2020-04-20 18:09:04 -0700

    user_spec.rb: Add password instantiation test for user input:
    ticket_spec.rb: Add belong_to tests for proper class relevance

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-
+  it {should belong_to(:organization)}
+  it {should belong_to(:region)}
+  it {should belong_to(:resource_category)}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+	
 	it {should belong_to(:organization)}
+
 	it "has an email" do
 		user = User.new
 		expect(user).to respond_to(:email)
+	end
+	it "has a password" do
+		user = User.new
+		expect(user).to respond_to(:password)
 	end
 end
