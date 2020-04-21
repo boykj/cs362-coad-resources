a9c77ab3587781a8dedc3d3e0ff4f99448084f29
boykj <jboyk918@gmail.com>
2020-04-20 18:22:12 -0700

    ticket_spec.rb: Add some basic attribute tests:
    Last commit before creating a branch for the rest of
    tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-
+	it "region has name" do
+		region = Region.new
+		expect(region).to respond_to(:name)
+  end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,5 +3,15 @@ require 'rails_helper'
 RSpec.describe Ticket, type: :model do
   it {should belong_to(:organization)}
   it {should belong_to(:region)}
-  it {should belong_to(:resource_category)}
+	it {should belong_to(:resource_category)}
+	
+	it "has a phone" do
+		ticket = Ticket.new
+		expect(ticket).to respond_to(:phone)
+	end
+
+	it "has a name" do
+		ticket = Ticket.new
+		expect(ticket).to respond_to(:name)
+	end
 end
