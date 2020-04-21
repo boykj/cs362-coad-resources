6ac950f6710b6515828bd63463aafeacccee3f85
boykj <jboyk918@gmail.com>
2020-04-20 18:32:09 -0700

    resource_cat_spec.rb: Add validation tests for relationships

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+	it {should have_many(:tickets)}
+	it {should have_and_belong_to_many(:organizations)}
+
 	it "has a name" do
 		resource = ResourceCategory.new()
 		expect(resource).to respond_to(:name)
