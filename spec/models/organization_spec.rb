f55df9fe08a3d34e5b084c1dd9a2e99213b7eccd
boykj <jboyk918@gmail.com>
2020-04-21 11:19:41 -0700

    organization_spec.rb: Refactor relationship tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+	let (:org) {Organization.new}
 
-  it {should have_many(:users)}
-  it {should have_many(:tickets)}
-	it {should have_and_belong_to_many(:resource_categories)}
-	
-	it "has a name, email, phone, status, primary_name, secondary_name, secondary_phone" do
-		org = Organization.new()
-		expect(org).to respond_to(:email)
-		expect(org).to respond_to(:name)
-		expect(org).to respond_to(:phone)
-		expect(org).to respond_to(:status)
-		expect(org).to respond_to(:primary_name)
-		expect(org).to respond_to(:secondary_name)
-		expect(org).to respond_to(:secondary_phone)
+	describe "relationships" do
+		it "has many users" do
+			expect(org).to have_many(:users)
+		end
+		it "has many tickets" do
+			expect(org).to have_many(:tickets)
+		end
 	end
 	
+	describe "properties" do
+		it "has a name" do
+			expect(org).to respond_to(:name)
+		end
+		it "has an email" do
+			expect(org).to respond_to(:email)
+		end
+		it "has a name" do
+			expect(org).to respond_to(:status)
+		end
+		it "has a name" do
+			expect(org).to respond_to(:primary_name)
+		end
+		it "has a name" do
+			expect(org).to respond_to(:secondary_name)
+		end
+		it "has a name" do
+			expect(org).to respond_to(:secondary_phone)
+		end
+	end
+		
 end
