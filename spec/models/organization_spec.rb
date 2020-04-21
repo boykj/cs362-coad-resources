f65a4c5cb167365bb387dedb2fb3c7b82b78fb80
boykj <jboyk918@gmail.com>
2020-04-21 08:07:02 -0700

    organization_spec.rb: Add instantiation tests
    Reference #01

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  it {should have_many(:users)}
+  it {should have_many(:tickets)}
+	it {should have_and_belong_to_many(:resource_categories)}
+	
+	it "has a name, email, phone, status, primary_name, secondary_name, secondary_phone" do
+		org = Organization.new()
+		expect(org).to respond_to(:email)
+		expect(org).to respond_to(:name)
+		expect(org).to respond_to(:phone)
+		expect(org).to respond_to(:status)
+		expect(org).to respond_to(:primary_name)
+		expect(org).to respond_to(:secondary_name)
+		expect(org).to respond_to(:secondary_phone)
+	end
+	
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,24 +6,12 @@ RSpec.describe Ticket, type: :model do
   it {should belong_to(:region)}
 	it {should belong_to(:resource_category)}
 	
-	it "has a phone" do
+	it "has a phone, name, region_Id, resource_category_id" do
 		ticket = Ticket.new
 		expect(ticket).to respond_to(:phone)
 		expect(ticket.phone_was) == true
-	end
-
-	it "has a name" do
-		ticket = Ticket.new
 		expect(ticket).to respond_to(:name)
-	end
-
-	it "has a region_id" do
-		ticket = Ticket.new
 		expect(ticket).to respond_to(:region_id)
-	end
-
-	it "has a resource_category_id" do
-		ticket = Ticket.new
 		expect(ticket).to respond_to(:resource_category_id)
 	end
 end
