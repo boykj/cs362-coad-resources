ca80eb4e2102b3f9336645f93278e9555f649557
boykj <jboyk918@gmail.com>
2020-04-21 11:46:15 -0700

    user_spec.rb: Add to_s method test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	let(:user) {User.new}
+	let(:user) {User.new(email: 'fakeyboi@googledatacollection.com')}
 	let(:user1) {User.new}
 
 	#it {should belong_to(:organization)
 
-	it "has an email" do
-		expect(user).to respond_to(:email)
+	describe "properties" do
+		it "has an email" do
+			expect(user).to respond_to(:email)
+		end
+		it "has a password" do
+			expect(user).to respond_to(:password)
+		end
 	end
-	it "has a password" do
-		expect(user).to respond_to(:password)
+
+	describe "#to_s" do
+		it "has a string that is the email" do
+			expected_email = 'fakeyboi@googledatacollection.com'
+			expect(user.to_s).to eq(expected_email)
+		end
 	end
 
 end
