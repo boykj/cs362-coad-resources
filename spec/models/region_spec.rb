17ab6dd3961487e892a123c47bab9b5ef18ac501
boykj <jboyk918@gmail.com>
2020-04-20 18:34:20 -0700

    region_spec.rb: Add validation test for many ticket relationship

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+	it {should have_many(:tickets)}
+
 	it "region has name" do
 		region = Region.new
 		expect(region).to respond_to(:name)
