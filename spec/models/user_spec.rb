81ba6e43bfe3adfeb3adcd6f39e4a274c5777c61
boykj <jboyk918@gmail.com>
2020-04-23 09:43:34 -0700

    user_spec.rb: Add validation tests for user and clean up code.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	let(:user) {User.new(email: 'fakeyboi@googledatacollection.com')}
-	let(:user1) {User.new}
+	let(:user) { User.new(email: 'fakeyboi@googledatacollection.com') }
+	let(:user1) { User.new }
 
 	#it {should belong_to(:organization)
 
@@ -10,9 +10,10 @@ RSpec.describe User, type: :model do
 		it "has an email" do
 			expect(user).to respond_to(:email)
 		end
-		it "has a password" do
-			expect(user).to respond_to(:password)
-		end
+	end
+
+	describe "relationships" do
+		specify {expect(user).to belong_to(:organization)}
 	end
 
 	describe "#to_s" do
@@ -22,4 +23,31 @@ RSpec.describe User, type: :model do
 		end
 	end
 
+	describe "vaidations" do
+		it "has an email" do
+			expect(user).to validate_presence_of(:email)
+		end
+
+		it "validates length of email" do
+			expect(user).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+		end
+
+		it "validates email format" do
+			#TODO
+		end
+
+		it "validates uniqueness of email" do
+			expect(user).to validate_uniqueness_of(:email).case_insensitive
+		end
+
+		it "validates presence of password" do
+			expect(user).to validate_presence_of(:password).on(:create)
+		end
+
+		it "validates length of password" do
+			expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255)
+		end
+	end
+
+	
 end
