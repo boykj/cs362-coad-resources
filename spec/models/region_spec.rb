162f7fb4f1a13e3a9c9ff3b51314e68a1f421527
boykj <jboyk918@gmail.com>
2020-04-27 18:01:46 -0700

    region_spec.rb: Accidental of edits on the wrong branch:
    Add validation tests for length of name on crea
    te and uniqueness of name

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -16,6 +16,15 @@ RSpec.describe Region, type: :model do
 	end
 
 	#validations
+	describe "validations" do
+		it "validates length of name on create" do
+			expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+		end
+
+		it "validates uniqueness of name, not case sensitive" do
+			expect(region).to validate_uniqueness_of(:name).case_insensitive
+		end
+	end
 
 	#methods
 	describe "#to_s" do
