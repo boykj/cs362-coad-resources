ef5f9d89bef5106c1326c8cd68517f4cdf7493ad
boykj <jboyk918@gmail.com>
2020-04-27 18:07:35 -0700

    resouce_cat_spec.rb: Add validation tests for length/uniqueness

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -16,4 +16,14 @@ RSpec.describe ResourceCategory, type: :model do
 		end
 	end
 
+	describe "validations" do
+		it "validates length of name on create" do
+			expect(resource_category).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+		end
+
+		it "validates uniqueness of name, not case sensitive" do
+			expect(resource_category).to validate_uniqueness_of(:name).case_insensitive
+		end
+	end
+
 end
