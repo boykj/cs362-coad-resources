ddeba6d57de1a43caf906e2f3e072cf2bf2363d5
boykj <jboyk918@gmail.com>
2020-05-03 17:12:55 -0700

    merge conflicts

--- factories/regions.rb
+++ factories/regions.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :region do
-<<<<<<< HEAD
-			name { 'Fake' }
-			trait :unspecified do
-				name { 'Unspecified' }
-			end
-=======
     name { 'Default' }
->>>>>>> 07_factory_refactors
   end
 end
