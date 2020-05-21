54612368280157c6c0a47bada65d27f0bb38be05
boykj <jboyk918@gmail.com>
2020-05-21 10:02:48 -0700

    Add basic test for create ticket:
    Start on the flow for closing a ticket

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
 
+	before :each do
+		@region = Region.create(name: 'Fake Region')
+		@resource_category = Resource_category.create(name: 'Fake Category')
+		@user = User.create(name: 'Fake name')
+	end
+
 end
--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+	before :each do
+		@region = Region.create(name: 'Fake Region')
+		@resource_category = ResourceCategory.create(name: 'Fake Category') 
+	end
+
+	it 'creates a ticket and display a success message' do
+		visit new_ticket_path
+		fill_in 'Full Name', with: 'Fake name'
+		fill_in 'Phone Number', with: '5410001234'
+		select @region.name, from: 'Region'
+		select @resource_category.name, from: 'Resource Category'
+		fill_in 'Description', with: 'Default test description'
+		click_on 'Send this help request'
+		expect(page).to have_content('Ticket Submitted')
+	end
+
 end
