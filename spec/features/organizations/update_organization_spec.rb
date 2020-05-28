53c20e2b614db7cbeacfa0766c0c7d1738143d2c
boykj <jboyk918@gmail.com>
2020-05-28 10:15:51 -0700

    update_org_spec: Fix org/admin login issue

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+	let(:admin) { create(:user, :admin) }
 	let(:organization) { create(:organization) }
 
 	context 'As an admin user do' 
 		it 'updates an organization' do
+			log_in_as(admin)
 			visit edit_organization_path(organization)
-			organization.stub(:name).and_return('Default fake name')
-			#click_on 'Save Changes'
-			#expect(page).to have_content('Default fake name')
+			#byebug
+			#click_on('Edit Organization')
+			#expect(current_path).to eq edit_organization_path(organization)
+			#fill_in 'Name', with: 'Default fake name'
+			#click_on 'Save'
+ 			#expect(page).to have_content('Default fake name')
 		end
 	end
 
 	context 'As an organization user' do
-		let(:user) { create(:user) }
 		let(:organization) { create(:organization, :approved) }
+		let(:user) { create(:user, organization: organization) }
 
 		before do
 			user.organization = organization
@@ -22,8 +27,10 @@ RSpec.describe 'Updating an Organization', type: :feature do
 		end
 
 		it 'updates an organization successfully' do
+			log_in_as(user)
 			visit edit_organization_path(organization)
 			#byebug
+			#click_on('Edit Organization')
 			#fill_in(organization.name).with('Default fake name')
 			#click_on 'Save Organization'
 			#expect(page).to have_content('Default fake name')
