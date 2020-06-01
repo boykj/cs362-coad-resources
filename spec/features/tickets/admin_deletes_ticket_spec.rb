ce484743efb44b3bbc88bf4680c74c1e34783b96
Isaiah Brickhouse <brickhoi@oregonstate.edu>
2020-06-01 15:37:00 -0700

    admin_deletes_ticket_spec.rb: add delete feature test

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
 
+	  let(:admin) { create(:user, :admin) }
+	  let(:ticket) { create(:ticket) }
+
+context 'as an admin' do 
+	it 'it deletes a ticket' do
+		log_in_as(admin)
+		visit ticket_path(ticket)
+		click_on 'Delete'
+			expect {
+				accept_alert "Are you sure? This cannot be undone." do
+					click_link 'OK'
+				end
+				sleep 1.second
+			}
+			visit dashboard_path()
+			expect(page).to_not have_content(ticket)
+		end
+	end
 end
