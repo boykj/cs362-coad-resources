36b8be3e9a25cad94162ad14151e8a5834e33ad8
boykj <jboyk918@gmail.com>
2020-06-01 14:28:22 -0700

    user_reg_spec: Add feature test for new user registration

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
-
+	let (:user) { create(:user) }
+	
+	context 'As a new user' do
+		it 'registers an account' do
+			visit new_user_registration_path()
+			fill_in 'Email', with: 'testemail@google.com'
+			fill_in 'Password', with: 'password'
+			fill_in 'Password confirmation', with: 'password'
+			click_button('Sign up')
+			expect(page).to have_content('Disaster Resource Network')
+		end
+	end
+	
 end
