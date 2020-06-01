466d7b826e3cb8f03c9b9f92924758b34888d1f6
Isaiah Brickhouse <brickhoi@oregonstate.edu>
2020-06-01 16:13:26 -0700

    capture_ticket_spec.rb: add capture test

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
 
+	let(:user) { create(:user, :organization_member) }
+	let(:organization) { create(:organization, :approved)}
+	let(:ticket) { create(:ticket) }
+
+	before do
+		user.organization = organization
+		ticket.name = 'default'
+		user.save 
+		ticket.save
+	end
+
+	context 'organization user' do
+		it 'it captures a ticket' do
+			log_in_as(user)
+			visit dashboard_path()
+			#byebug
+			click_link 'Tickets'
+			#expect(ticket.captured).to be_truthy
+			click_on 'default'
+			click_on 'Capture'
+		end
+	end
 end
