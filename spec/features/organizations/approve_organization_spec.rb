38ee4bd27d9988521f7bf9aa5f7f0db3a7fb6685
boykj <jboyk918@gmail.com>
2020-06-01 11:40:07 -0700

    approve_org_spec: Add feature test for approving organization

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  let(:admin) { create(:user, :admin) }
+  let(:organization) { create(:organization) }
 
+  context 'As an admin user' do
+		it 'approved an organization' do
+			log_in_as(admin)
+			visit organizations_path(organization)
+			click_on 'Pending'
+			click_on 'Review'
+			click_on 'Approve'
+			expect(page).to have_content(organization)
+		end
+	end
 end
--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+  let(:admin) { create(:user, :admin) }
+  let(:organization) { create(:organization) }
 
+  context 'As an admin user' do
+    it 'approved an organization' do
+        log_in_as(admin)
+        visit organizations_path(organization)
+        click_on 'Pending'
+				click_on 'Review'
+				fill_in 'Rejection Reason', with: 'This organization is hot garbage'
+        click_on 'Reject'
+        expect(page).to have_content('This organization is hot garbage')
+    end
+  end
 end
