4f1d35e89c5fcc745ef697ea1db97f3a7e6b01ee
boykj <jboyk918@gmail.com>
2020-06-01 11:50:33 -0700

    user_login_spec: Add feature test for org user login

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+	let(:user) { create(:user, :organization_member) }
+
+	context 'As an organization user' do
+		it 'logs in' do
+			log_in_as(user)
+		 	expect(page).to have_content('Dashboard')
+		end
+	end
 
 end
