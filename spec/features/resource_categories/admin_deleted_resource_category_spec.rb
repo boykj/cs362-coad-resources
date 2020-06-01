fb9974434123675d3223ccb677fe7d18472cb166
boykj <jboyk918@gmail.com>
2020-06-01 15:00:05 -0700

    delete_resource_cat: Add feature test for resource cat. delete:
    Alert window took a minute to figure out. Had to take it
    to stack overflow to figure out the problem

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
-
+  let(:admin) { create(:user, :admin) }
+	let(:resource_category) { create(:resource_category) }
+	
+	context 'As an admin' do
+		it 'deletes a resource category' do
+			log_in_as(admin)
+			visit resource_category_url(resource_category)
+			click_on 'Delete'
+			expect {
+				accept_alert "Are you sure? This cannot be undone. Any tickets will have an Unspecified resource category." do
+					click_link 'OK'
+				end
+				sleep 1.second
+			}
+			visit resource_categories_path
+			expect(page).to_not have_content(resource_category)
+		end
+	end
+	
 end
