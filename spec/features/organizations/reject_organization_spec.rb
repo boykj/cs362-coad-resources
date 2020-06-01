bd8141533fbb6b523a1d440e684e4230dd635a51
boykj <jboyk918@gmail.com>
2020-06-01 15:03:16 -0700

    reject_org_spec: Formatting changes - indents

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'Rejecting an organization', type: :feature do
 
   context 'As an admin user' do
     it 'approved an organization' do
-        log_in_as(admin)
-        visit organizations_path(organization)
-        click_on 'Pending'
-				click_on 'Review'
-				fill_in 'Rejection Reason', with: 'This organization is hot garbage'
-        click_on 'Reject'
-        expect(page).to have_content('This organization is hot garbage')
+      log_in_as(admin)
+      visit organizations_path(organization)
+      click_on 'Pending'
+			click_on 'Review'
+			fill_in 'Rejection Reason', with: 'This organization is hot garbage'
+      click_on 'Reject'
+      expect(page).to have_content('This organization is hot garbage')
     end
   end
 end
